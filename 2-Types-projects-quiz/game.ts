7dae1e21974f30046c50adde1bfd34ffa98031c8
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-05-02 20:03:01 +0900

    Implements : move Game

--- 2-Types-projects-quiz/game.ts
+++ 2-Types-projects-quiz/game.ts
@@ -1,12 +1,43 @@
 /**
  * Let's make a game 🕹
  */
+
+interface position {
+  x: number;
+  y: number;
+}
+
+let position: position = {
+  x: 0,
+  y: 0,
+};
+
+type moved = "up" | "down" | "left" | "right";
+const move = (Moved: moved) => {
+  switch (Moved) {
+    case "up":
+      position.y++;
+      break;
+    case "down":
+      position.y--;
+      break;
+    case "left":
+      position.x--;
+      break;
+    case "right":
+      position.x++;
+      break;
+    default:
+      console.log("다시입력하세요");
+  }
+};
+
 console.log(position); // { x: 0, y: 0}
-move('up');
+move("up");
 console.log(position); // { x: 0, y: 1}
-move('down');
+move("down");
 console.log(position); // { x: 0, y: 0}
-move('left');
+move("left");
 console.log(position); // { x: -1, y: 0}
-move('right');
+move("right");
 console.log(position); // { x: 0, y: 0}
