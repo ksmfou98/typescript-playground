d551a0e32cb15aee82c0987ab92f231cb18f01c4
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-05-15 16:00:04 +0900

    3-4-abstraction.ts

---
+++ 3-oop/3-4-abstraction.ts
@@ -0,0 +1,66 @@
+{
+  /**
+   * encapsulation 으로 외부에서 보이면 안될 값들을 설정할 수 있다
+   */
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+
+  interface CoffeeMaker{
+      makeCoffee(shots: number): CoffeeCup;
+  }
+
+  class CoffeeMachine implements CoffeeMaker {
+    // 클래스 안에서 변수를 선언할때는 const, let을 사용하지 않음
+    private static BEANS_GRAMM_PER_SHOT: number = 7; // static을 붙이면 class level 이 됨   class level : class 와 연결이 되어 있기 때문에 object마다 생성되지않음
+    private coffeeBeans: number = 0; // instance (or object) level
+
+    private constructor(coffeeBeans: number) {
+      // constructor 은 생성자로 클래스를 호출할때마다 실행되는 함수임
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    static makeMachine(coffeeBeans: number): CoffeeMachine {
+      return new CoffeeMachine(coffeeBeans);
+    }
+
+    fillCoffeeBeans(beans: number) {
+      if (beans < 0) {
+        throw new Error("value for beans should be greater than 0");
+      }
+      this.coffeeBeans += beans;
+    }
+
+    private grindBeans(shots: number) {
+      console.log(`grinding beans for ${shots}`);
+      if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
+        throw new Error("Not enough coffee beans!");
+      }
+      this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
+    }
+
+    private extract(shots: number): CoffeeCup {
+      console.log(`Pulling ${shots} shots ... ☕️`);
+      return {
+        shots,
+        hasMilk: false,
+      };
+    }
+
+    private preheat(): void {
+      console.log("heating up...🔥 ");
+    }
+
+    makeCoffee (shots: number): CoffeeCup  {
+      this.grindBeans(shots);
+      this.preheat();
+      return this.extract(shots);
+      
+    };
+  }
+
+  const maker = CoffeeMachine.makeMachine(32); // 여기 괄호 안에는 생성자에 값을 넘겨주는 값을 적으면 됨
+  maker.fillCoffeeBeans(10);
+  
+}
