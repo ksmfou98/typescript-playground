dc54453c0b4fed1153369fd450c634d0f601f4c6
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-06-30 20:44:54 +0900

    4-1-function

---
+++ 4-generic/4-1-function.ts
@@ -0,0 +1,18 @@
+{
+  function checkNotNullBad(arg: number | null): number {
+    if (arg === null) {
+      throw new Error("not valid number");
+    }
+    return arg;
+  }
+
+  function checkNotNull<T>(arg: T | null): T {
+    if (arg === null) {
+      throw new Error("not valid number");
+    }
+    return arg;
+  }
+
+  const number = checkNotNull(123);
+  const boal: boolean = checkNotNull(true);
+}
